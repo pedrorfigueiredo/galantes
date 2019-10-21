4f4e612c304c17c105a792db0dab35779433fb99
Dezivo <dezivo.studios@gmail.com>
2019-10-21 11:13:40 -0300

    add description in card

--- routes/index.js
+++ routes/index.js
@@ -74,9 +74,10 @@ router.post("/", upload.single("image"), middleware.isLoggedIn, function(req, re
         var image = result.secure_url;
         var imageId = result.public_id
         var title = req.body.title;
+        var description = req.body.description;
         var tag1 = req.body.tag1.toLowerCase();
         var tag2 = req.body.tag2.toLowerCase();
-        var newCloth = {title:title, tag1:tag1, tag2:tag2, image:image, imageId: imageId};
+        var newCloth = {title:title, description:description, tag1:tag1, tag2:tag2, image:image, imageId: imageId};
         // Create a new cloth and save to DB
         Cloth.create(newCloth, function(err, newlyCreated){
             if(err){
