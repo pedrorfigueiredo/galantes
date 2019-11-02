17856c83bc6fa6b975025c9c6e5c1dd49d38934b
Dezivo <dezivo.studios@gmail.com>
2019-11-01 23:04:17 -0300

    change fechar button to red

--- seedUser.js
+++ seedUser.js
@@ -1,17 +1,17 @@
-// var mongoose = require("mongoose");
-// var User = require("./models/user");
+var mongoose = require("mongoose");
+var User = require("./models/user");
 
-// function seedUser(){
-//     var newUser = new User({username: "admin"});
-//     User.register(newUser, "admin", function(err, user){
-//         if(err){
-//             console.log(err.message);
-//             return res.render("/");
-//         }
-//         passport.authenticate("local")(req, res, function(){
-//             console.log("User Registered");
-//         });
-//     });
-// }
+function seedUser(){
+    var newUser = new User({username: "vistase"});
+    User.register(newUser, "230417valentina", function(err, user){
+        if(err){
+            console.log(err.message);
+            return res.render("/");
+        }
+        passport.authenticate("local")(req, res, function(){
+            console.log("User Registered");
+        });
+    });
+}
 
-// module.exports = seedUser;
+module.exports = seedUser;
